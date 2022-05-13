f8697a4f48c39082cb94aedf408c2d8cea242cfe
swathy <105238915+swathykeerthana@users.noreply.github.com>
2022-05-13 12:37:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 import pandas
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
