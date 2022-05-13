fab7983709c3ca0029c844acdbe4eab11f84a875
swathy <105238915+swathykeerthana@users.noreply.github.com>
2022-05-13 12:30:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,15 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
-fruits_selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado','strawberries'])
-fruits_to_show= my_fruit_list.loc[fruits_selected]
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('fruit')
 
-streamlit.dataframe(fruits_to_show)
+#import pandas
+#fruit_selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado','strawberries'])
+#fruits_to_show= my_fruit_list.loc[fruit_selected]
+
+streamlit.dataframe(my_fruit_list)
+
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
