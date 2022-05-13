7960f5baa8e5b8d12b5b5af1e627bf376a73c391
swathy <105238915+swathykeerthana@users.noreply.github.com>
2022-05-13 12:34:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#streamlit.dataframe(fruityvice_normalized)
+import pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
 
 
